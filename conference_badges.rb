91300d5da879ce49ae74cf40bfe7c72cf17b5acf
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-28 21:07:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,8 +9,8 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(array)
-  array.each_with_index {|name, index|}
+  array.each_with_index {|name, index| binding.pry }
   "Hello, #{name}! You'll be assigned to room #{index}!"
-  binding.pry
+  
 
 end
