99e738f2d785c04ecf4f855239c1ee153781e47e
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-28 21:12:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,9 @@ end
 
 def assign_rooms(array)
   array.each_with_index do|name, index|
-    "Hello, #{name}! You'll be assigned to room #{index+1}!"
+    
+    array.collect do |name| "Hello, #{name}! You'll be assigned to room #{index+1}!"
+    end
     # binding.pry
   end
 
