3e44dd87fcd81be305dcb782f2834f7bad3d6592
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-28 21:53:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,8 +17,8 @@ def assign_rooms(array)
 
 def printer(names)
   names_copy = names
-  puts batch_badge_creator(names_copy)
+  batch_badge_creator(names_copy)
   binding.pry
-  puts assign_rooms(names)
+  assign_rooms(names)
 
 end
